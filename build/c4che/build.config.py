afee7586ed8d384870a1fe2f0c5f1a18c5d23912
smallville7123 <smallville7123@gmail.com>
2020-12-22 23:56:27 +1000

    first commit

---
+++ build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x2001300
+tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'c', 'tooldir': None, 'funs': None}, {'tool': 'clang', 'tooldir': None, 'funs': None}, {'tool': 'compiler_c', 'tooldir': None, 'funs': None}, {'tool': 'lv2', 'tooldir': None, 'funs': None}, {'tool': 'clang_compilation_database', 'tooldir': None, 'funs': None}, {'tool': 'autowaf', 'tooldir': None, 'funs': None}]
